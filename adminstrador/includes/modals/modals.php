348a04705fca00ca29402d1f1f4f286e0b71abb4
AngelesZP <149276705+SandraZP@users.noreply.github.com>
2024-04-01 21:32:38 -0600

    formulario
    usuario

--- adminstrador/includes/modals/modals.php
+++ adminstrador/includes/modals/modals.php
@@ -9,20 +9,58 @@
         </button>
       </div>
       <div class="modal-body">
-        <form>
+        <form  id="formAdmin"  name="formAdmin" >
           <div class="form-group">
-            <label for="recipient-name" class="col-form-label">Recipient:</label>
-            <input type="text" class="form-control" id="recipient-name">
+            <label for="control-label">Nombre</label>
+            <input type="text" class="form-control" name="nombre" id="nombre">
           </div>
+
           <div class="form-group">
-            <label for="message-text" class="col-form-label">Message:</label>
-            <textarea class="form-control" id="message-text"></textarea>
+            <label for="control-label">Primer Apellido</label>
+            <input type="text" class="form-control" name="apellido_paterno" id="apellido_paterno">
           </div>
-        </form>
-      </div>
-      <div class="modal-footer">
+
+          <div class="form-group">
+            <label for="control-label">Segundo Apellido</label>
+            <input type="text" class="form-control" name="apellido_materno" id="apellido_materno">
+          </div>
+
+          <div class="form-group">
+            <label for="control-label">Usuario</label>
+            <input type="text" class="form-control" name="usuario" id="usuario">
+          </div>
+
+          <div class="form-group">
+            <label for="control-label">Sexo</label>
+            <input type="text" class="form-control" name="sexo" id="sexo">
+          </div>
+          
+          
+          <div class="form-group">
+            <label for="control-label">Contraseña</label>
+            <input type="password" class="form-control" name="password" id="password">
+          </div>
+
+          <div class="form-group">
+            <label for="listRol">Rol</label>
+            <select  class="form-control"name="listRol" id="listRol">
+              <option value="1">Adminstrador</option>
+              <option value="2">Asistente</option>
+            </select>
+          </div>
+
+          <div class="form-group">
+            <label for="listRol">Estado</label>
+            <select  class="form-control"name="listRol" id="listRol">
+              <option value="1">Activo</option>
+              <option value="2">Inactivo</option>
+            </select>
+          </div>
+          <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-        <button type="button" class="btn btn-primary">Guardar</button>
+        <button type="button" class="btn btn-primary" type="subtmi">Guardar</button>
+      </div>
+        </form>
       </div>
     </div>
   </div>
