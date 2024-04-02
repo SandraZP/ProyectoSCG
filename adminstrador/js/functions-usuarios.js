cbf170ee74a21704ab07747e4496904d4965f0ba
AngelesZP <149276705+SandraZP@users.noreply.github.com>
2024-04-01 22:48:27 -0600

    dataTable
    data

--- adminstrador/js/functions-usuarios.js
+++ adminstrador/js/functions-usuarios.js
@@ -1,6 +1,36 @@
  $('#usuarios').DataTable();
 
 
+document.AddEventListener('DOMcontentLoaded',function(){
+    usuarios=$('#usuarios').DataTable({
+        "aProcessing": true,
+        "aServerSide": true,
+        "language":{
+            "url":"//cdn.datatables.net/plug-ins/1.10.20/Spanish.json"
+        },
+        "ajax":{
+            "url":"./models/usuarios/table_usuarios.php",
+            "dataSrc":""
+        },
+        "columns":[
+            {"data":"acciones"},
+            {"data":"idusuario"},
+            {"data":"nombre"},
+            {"data":"apellido_paterno"},
+            {"data":"apellido_materno"},
+            {"data":"usuario"},
+            {"data":"sexo"},
+            {"data":"correo"},
+            {"data":"rol"}
+        ],
+        "responsive": true,
+        "bDestroy":true,
+        "iDisplayLength":10,
+        "order":[[0,"asc"]]
+
+    })
+})
+
 
 
  function openModal(){
